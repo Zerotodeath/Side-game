7f173c6ace95ff151f86fcaf4003cb0daf1f62e0
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-02-08 16:10:05 -0500

    Made it work

--- Side game/AI_one.gd
+++ Side game/AI_one.gd
@@ -5,6 +5,8 @@ onready var may_chase = get_tree().get_nodes_in_group("AI")
 
 var run_speed = 40
 var velocity = Vector2.ZERO
+var emeny
+
 
 onready var In_area = $Area2D.get_overlapping_areas()
 var can_chase = true
@@ -14,16 +16,18 @@ func _ready():
 
 
 func _physics_process(delta):
-	print(can_chase)
-	for area in In_area:
-		if area == "Area2D":
-			if is_in_group("AI"):
-				can_chase = true
-		else:
-			can_chase = false
-		
 	
-	if can_chase == true:
-		velocity = position.direction_to(A2.position) * run_speed
+	if emeny != null:
+		velocity = position.direction_to(emeny.global_position) * run_speed
 	velocity = move_and_slide(velocity)
 
+
+
+func _on_Area2D_area_entered(area):
+	emeny = area.get_parent()
+	can_chase = true
+
+
+func _on_Area2D_area_exited(area):
+	can_chase = false
+	emeny = null
