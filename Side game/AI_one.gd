ef236241a04b6748fc93451d122090fde6a283f8
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-02-08 12:29:02 -0500

    Made 1 node move

--- Side game/AI_one.gd
+++ Side game/AI_one.gd
@@ -1,7 +1,29 @@
 extends KinematicBody2D
 
+onready var A2 = get_parent().get_node("AI 2")
+onready var may_chase = get_tree().get_nodes_in_group("AI")
 
+var run_speed = 40
+var velocity = Vector2.ZERO
+
+onready var In_area = $Area2D.get_overlapping_areas()
+var can_chase = true
 
 func _ready():
-	print(get_parent().get_node("AI 2").position)
-	print(position)
+	pass
+
+
+func _physics_process(delta):
+	print(can_chase)
+	for area in In_area:
+		if area == "Area2D":
+			if is_in_group("AI"):
+				can_chase = true
+		else:
+			can_chase = false
+		
+	
+	if can_chase == true:
+		velocity = position.direction_to(A2.position) * run_speed
+	velocity = move_and_slide(velocity)
+
