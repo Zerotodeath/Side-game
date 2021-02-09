856c19270c8d8c1f7d2d7afe5a65c427cba8ef7a
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-02-09 13:54:44 -0500

    made better code

--- Side game/AI_one.gd
+++ Side game/AI_one.gd
@@ -5,7 +5,9 @@ enum{
 	Chaseing
 }
 
-onready var may_chase = get_tree().get_nodes_in_group("AI")
+onready var hithurtbox = $hithurtbox.get_overlapping_areas()
+
+export var can_attack = true
 
 var run_speed = 40
 var velocity = Vector2.ZERO
@@ -14,8 +16,11 @@ var target
 var fighting = false
 var eating
 
-var hungry = 49
+export var health = 100
+export var damage = 25
 var searching = true
+
+
 
 
 onready var In_area = $Area2D.get_overlapping_areas()
@@ -24,23 +29,42 @@ var can_chase = true
 func _ready():
 	$Area2D.connect("area_entered", self, "_on_Area2D_area_entered")
 	$Area2D.connect("area_exited", self, "_on_Area2D_area_exited")
+	$hithurtbox.connect("area_entered", self, "_on_hithurtbox_area_entered")
+	$hithurtbox.connect("area_exited", self, "_on_hithurtbox_area_exited")
+	$Timer.connect("timeout", self, "hit_timer")
 func _physics_process(delta):
 	print(eating)
+	if health < 0:
+		queue_free()
 	if target != null:
 		velocity = position.direction_to(target.global_position) * run_speed
 	velocity = move_and_slide(velocity)
-
+	
 
 func _on_Area2D_area_entered(area):
 	target = area.get_parent()
 	can_chase = true
 	if target.is_in_group("AI"):
 		fighting = true
-	elif target.is_in_group("Plant"):
-		eating = true
+	
 
 
 func _on_Area2D_area_exited(area):
 	can_chase = false
 	target = null
 	fighting = false
+
+
+func _on_hithurtbox_area_entered(area):
+	target = area.get_parent()
+	if target.is_in_group("Plant"):
+		eating = true
+	$Timer.start()
+
+func _on_hithurtbox_area_exited(area):
+	eating = false
+	$Timer.stop()
+
+func hit_timer():
+	target.health -= damage
+
