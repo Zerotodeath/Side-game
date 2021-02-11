09447cd18ec27a357b6eaa90a4b9d470ce509bc0
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-02-11 12:31:56 -0500

    made base

---
+++ Side project/AI.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+onready var Nav = $Navigation2D
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
